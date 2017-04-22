5fc5e1f0536c1aaf2e9d18db9298e2cd4f377d6f
Jayesh Tambe <tambe_jayesh@hotmail.com>
2017-04-21 21:05:15 -0700

    Adding map and key in tablerow function

--- reactapp/main.js
+++ reactapp/main.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App.jsx';
+import App2 from './App2.jsx';
 
 ReactDOM.render(<App/>, document.getElementById('app'));
+
+// added a component with table row and iterating json object
+ReactDOM.render(<App2/>, document.getElementById('app2'));
